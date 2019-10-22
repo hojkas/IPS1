73fb22b701b54824f4f646982b2fbecd46c33b18
hojkas <ivka.strnadova@gmail.com>
2019-10-22 13:07:19 +0200

    no real changes

--- test_mmal.c
+++ test_mmal.c
@@ -67,7 +67,6 @@ int main()
     assert(first_arena->next == NULL);
     assert(first_arena->size > 0);
     assert(first_arena->size <= PAGE_SIZE);
-    printf("I'm here");
     Header *h1 = (Header*)(&first_arena[1]);
     Header *h2 = h1->next;
     assert(h1->asize == 42);
@@ -81,6 +80,7 @@ int main()
     // Druha alokace
 
     char *p2 = mmalloc(42);
+    
     /**
      *   v----- first_arena
      *   +-----+------+----+------+----+------+----------------+
